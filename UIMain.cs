9029fef88c5d83c449ced494d1cdad38ca2865a3
beyzakgul <beyzanurakgul@hotmail.com>
2022-11-04 15:29:40 +0300

    Resimler ve ilk kod yüklendi

---
+++ UIMain.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIMain : MonoBehaviour
+{
+
+    [Header("UIPages")]
+    public GameObject settingsScreen;
+    public GameObject creditsScreen;
+    public GameObject mainScreen;
+
+    public void StartGame()
+    {
+        //Oyunu ba�latacak k�s�m.
+        Application.LoadLevel("level�smi");
+    }
+
+    public void Settings()
+    {
+        mainScreen.SetActive(false);
+        settingsScreen.SetActive(true);
+    }
+    public void set2Menu()
+    {
+        mainScreen.SetActive(true);
+        settingsScreen.SetActive(false);
+    }
+    public void Credits()
+    {
+        mainScreen.SetActive(false);
+        creditsScreen.SetActive(true);
+    }
+    public void cre2Menu()
+    {
+        mainScreen.SetActive(true);
+        creditsScreen.SetActive(false);
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+}
